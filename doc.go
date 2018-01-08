f37e7ddf879dfc95959f0d6828efb3655bf7568b
hex7c0 <carniellifrancesco@gmail.com>
2018-01-08 20:51:33 +0100

    main: add doc helper WIP

---
+++ doc.go
@@ -0,0 +1,13 @@
+// Package config implements methods for get config variables.
+//
+// configuration contains the following packages:
+//
+// The file package provides a comprehensive set of utility functions about files.
+//
+package configuration
+
+// blank imports help docs.
+import (
+	// file package
+	_ "github.com/supermercato24/configuration/file"
+)
